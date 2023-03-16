913f5e4f72228e9917528d90ad08ec037523753d
Nkiruka2 <nwoga.ruth29@gmail.com>
2023-03-16 13:29:33 -0700

    My first c script

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * master - A programm that prints with puts function
+ * Return: 0 (good)
+ */
+int master(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}

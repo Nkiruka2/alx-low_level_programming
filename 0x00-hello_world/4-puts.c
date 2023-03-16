b32233639cfbb7d06542e19f588fac1438e161e1
Nkiruka2 <nwoga.ruth29@gmail.com>
2023-03-16 13:40:58 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 /**
- * master - A programm that prints with puts function
- * Return: 0 (good)
+ * main - A programm that prints with puts function
+ * Return: 0 (Success)
  */
-int master(void)
+int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
